26438df824d9e3948607a7e6495ba1a4aa06d577
James W Moody <james@wmoody.me>
2020-05-25 10:28:07 -0700

    Add GraphQL

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PostViewer from './PostViewer';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { apiResponse: '' };
-    }
-
-    callAPI() {
-        fetch('http://localhost:9000/testAPI')
-            .then((res) => res.text())
-            .then((res) => this.setState({ apiResponse: res }));
-    }
-
-    componentDidMount() {
-        this.callAPI();
-    }
-
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>{this.state.apiResponse}</p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
+            <main>
+                <PostViewer />
+            </main>
         );
     }
 }
---
+++ src/PostViewer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { Table } from 'reactstrap';
+
+export const GET_POSTS = gql`
+    query GetPosts {
+        posts {
+            id
+            author
+            body
+            createdAt
+        }
+    }
+`;
+
+export default () => (
+    <Query query={GET_POSTS}>
+        {({ loading, data }) =>
+            !loading && (
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>Author</th>
+                            <th>Body</th>
+                            <th>Created At</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.posts.map((post) => (
+                            <tr key={post.id}>
+                                <td>{post.author}</td>
+                                <td>{post.body}</td>
+                                <td>{post.createdAt}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            )
+        }
+    </Query>
+);
